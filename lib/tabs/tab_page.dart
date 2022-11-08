6e431e48265e31fd8a6e5c7aa8577ab89cd7fe86
J-P Nurmi <jp.nurmi@canonical.com>
2022-11-08 14:08:08 +0100

    Extract tab bar & indexed stack

--- lib/tabs/tab_page.dart
+++ lib/tabs/tab_page.dart
@@ -19,106 +19,116 @@ import 'tab_model.dart';
 class TabPage extends StatelessWidget {
   const TabPage({super.key});
 
-  static Widget create(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => TabModel(),
-      child: const TabPage(),
+      builder: (context, child) => TabCommands(
+        child: TabActions(
+          child: Focus(
+            autofocus: true,
+            child: Scaffold(
+              appBar: WindowTitleBar(
+                titleSpacing: 0,
+                centerTitle: false,
+                title: const _TabBar(),
+              ),
+              body: const _TabStack(),
+            ),
+          ),
+        ),
+      ),
     );
   }
+}
+
+class _TabStack extends StatelessWidget {
+  const _TabStack();
+
+  @override
+  Widget build(BuildContext context) {
+    final model = context.watch<TabModel>();
+    return IndexedStack(
+      index: model.currentIndex,
+      children: [
+        for (final tab in model.tabs)
+          ChangeNotifierProvider.value(
+            value: tab,
+            key: ValueKey(tab),
+            builder: (context, child) {
+              final tab = context.watch<TabItem>();
+              return FocusScope(
+                node: tab.focusScope,
+                child: tab.instance == null
+                    ? HomePage(
+                        onSelected: (instance) => tab.instance = instance,
+                      )
+                    : TerminalPage(
+                        instance: tab.instance!,
+                        onExit: () => tab.instance = null,
+                      ),
+              );
+            },
+          ),
+      ],
+    );
+  }
+}
+
+class _TabBar extends StatelessWidget {
+  const _TabBar();
 
   @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context);
     final model = context.watch<TabModel>();
-    return TabCommands(
-      child: TabActions(
-        child: Focus(
-          autofocus: true,
-          child: Scaffold(
-            appBar: WindowTitleBar(
-              titleSpacing: 0,
-              centerTitle: false,
-              title: Builder(
-                builder: (context) => MovableTabBar(
-                  count: model.tabs.length,
-                  builder: (context, index) {
-                    return ChangeNotifierProvider.value(
-                      value: model.tabs[index],
-                      builder: (context, child) {
-                        final tab = context.watch<TabItem>();
-                        return MovableTabButton(
-                          selected: index == model.currentIndex,
-                          onPressed: () => model.currentIndex = index,
-                          onClosed: Actions.handler(
-                            context,
-                            CloseTabIntent(index),
-                          ),
-                          icon: tab.instance != null
-                              ? OsLogo.asset(
-                                  name: tab.instance!.os,
-                                  size: 32,
-                                )
-                              : const SizedBox(width: 8),
-                          label: Text(tab.instance?.name ?? l10n.homeTab),
-                        );
-                      },
-                    );
-                  },
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        icon: const Icon(YaruIcons.plus),
-                        iconSize: 16,
-                        padding: EdgeInsets.zero,
-                        visualDensity:
-                            const VisualDensity(horizontal: -4, vertical: -4),
-                        onPressed:
-                            Actions.handler(context, const AddTabIntent()),
-                      ),
-                      const SizedBox(width: 2),
-                      QuickMenuButton(
-                        onSelected: (instance) {
-                          Actions.maybeInvoke(context, AddTabIntent(instance));
-                        },
-                      ),
-                      const SizedBox(width: kMinInteractiveDimension),
-                    ],
-                  ),
-                  onMoved: (from, to) {
-                    Actions.invoke(context, MoveTabIntent(from, to));
-                  },
-                ),
+    return MovableTabBar(
+      count: model.tabs.length,
+      builder: (context, index) {
+        return ChangeNotifierProvider.value(
+          value: model.tabs[index],
+          builder: (context, child) {
+            final tab = context.watch<TabItem>();
+            return MovableTabButton(
+              selected: index == model.currentIndex,
+              onPressed: () => model.currentIndex = index,
+              onClosed: Actions.handler(
+                context,
+                CloseTabIntent(index),
               ),
-            ),
-            body: IndexedStack(
-              index: model.currentIndex,
-              children: [
-                for (final tab in model.tabs)
-                  ChangeNotifierProvider.value(
-                    value: tab,
-                    key: ValueKey(tab),
-                    builder: (context, child) {
-                      final tab = context.watch<TabItem>();
-                      return FocusScope(
-                        node: tab.focusScope,
-                        child: tab.instance == null
-                            ? HomePage(
-                                onSelected: (instance) =>
-                                    tab.instance = instance,
-                              )
-                            : TerminalPage(
-                                instance: tab.instance!,
-                                onExit: () => tab.instance = null,
-                              ),
-                      );
-                    },
-                  ),
-              ],
-            ),
+              icon: tab.instance != null
+                  ? OsLogo.asset(
+                      name: tab.instance!.os,
+                      size: 32,
+                    )
+                  : const SizedBox(width: 8),
+              label: Text(tab.instance?.name ?? l10n.homeTab),
+            );
+          },
+        );
+      },
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            icon: const Icon(YaruIcons.plus),
+            iconSize: 16,
+            padding: EdgeInsets.zero,
+            visualDensity: const VisualDensity(horizontal: -4, vertical: -4),
+            onPressed: Actions.handler(context, const AddTabIntent()),
           ),
-        ),
+          const SizedBox(width: 2),
+          QuickMenuButton(
+            onSelected: (instance) {
+              Actions.maybeInvoke(context, AddTabIntent(instance));
+            },
+          ),
+          const SizedBox(width: kMinInteractiveDimension),
+        ],
       ),
+      onMoved: (from, to) {
+        Actions.invoke(context, MoveTabIntent(from, to));
+      },
     );
   }
 }
