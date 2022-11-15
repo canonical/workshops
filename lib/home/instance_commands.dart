9a9e8ef05e0a38b09faab4daae94d85b75017b68
J-P Nurmi <jp.nurmi@canonical.com>
2022-11-15 15:24:05 +0100

    Add commands for managing instances
    Ref: #121

---
+++ lib/home/instance_commands.dart
@@ -0,0 +1,87 @@
+import 'package:command_store/command_store.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
+
+import '../tabs/tab_intents.dart';
+import 'instance_intents.dart';
+import 'instance_store.dart';
+
+class InstanceCommands extends StatelessWidget {
+  const InstanceCommands({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context);
+    final store = context.watch<InstanceStore>();
+    return CommandScope(
+      commands: [
+        if (store.instances.value?.isNotEmpty == true) ...[
+          Command(
+            id: 'instance-launch',
+            label: l10n.launchInstanceCommand,
+            children: store.instances.value
+                ?.map((i) => Command(
+                    id: 'instance-launch-$i',
+                    label: i,
+                    intent: VoidCallbackIntent(() {
+                      Actions.invoke(
+                        primaryFocus!.context!,
+                        AddTabIntent(store.getInstance(i)),
+                      );
+                    })))
+                .toList(),
+          ),
+          Command(
+            id: 'instance-start',
+            label: l10n.startInstanceCommand,
+            children: store.instances.value
+                ?.map((i) => Command(
+                    id: 'instance-start-$i',
+                    label: i,
+                    intent: VoidCallbackIntent(() {
+                      Actions.invoke(
+                        primaryFocus!.context!,
+                        StartInstanceIntent(store.getInstance(i)),
+                      );
+                    })))
+                .toList(),
+          ),
+          Command(
+            id: 'instance-stop',
+            label: l10n.stopInstanceCommand,
+            children: store.instances.value
+                ?.map((i) => Command(
+                    id: 'instance-stop-$i',
+                    label: i,
+                    intent: VoidCallbackIntent(() {
+                      Actions.invoke(
+                        primaryFocus!.context!,
+                        StopInstanceIntent(store.getInstance(i)),
+                      );
+                    })))
+                .toList(),
+          ),
+          Command(
+            id: 'instance-delete',
+            label: l10n.deleteInstanceCommand,
+            children: store.instances.value
+                ?.map((i) => Command(
+                    id: 'instance-delete-$i',
+                    label: i,
+                    intent: VoidCallbackIntent(() {
+                      Actions.invoke(
+                        primaryFocus!.context!,
+                        DeleteInstanceIntent(store.getInstance(i)),
+                      );
+                    })))
+                .toList(),
+          ),
+        ],
+      ],
+      child: child,
+    );
+  }
+}
