5564dd58bfc8e358479d78168b0b3c41a9cde702
J-P Nurmi <jpnurmi@gmail.com>
2022-11-20 20:32:04 +0100

    Initial implementation

--- lib/jsettings.dart
+++ lib/jsettings.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library jsettings;
 
-export 'src/jsettings_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/jsettings.dart';
---
+++ lib/src/jsettings.dart
@@ -0,0 +1,149 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:collection/collection.dart';
+import 'package:file/file.dart';
+import 'package:file/local.dart';
+import 'package:meta/meta.dart';
+import 'package:watcher/watcher.dart';
+
+class JSettings {
+  JSettings(this._path);
+
+  final String _path;
+  bool? _invalid;
+  DateTime? _timestamp;
+  StreamSubscription? _watcher;
+  Map<String, Object>? _values;
+  final _added = StreamController<String>.broadcast();
+  final _changed = StreamController<String>.broadcast();
+  final _removed = StreamController<String>.broadcast();
+
+  Stream<String> get added => _added.stream;
+  Stream<String> get changed => _changed.stream;
+  Stream<String> get removed => _removed.stream;
+
+  @visibleForTesting
+  FileSystem fs = const LocalFileSystem();
+
+  Future<void> init() async {
+    _watcher ??= FileWatcher(_path).events.listen((event) async {
+      switch (event.type) {
+        case ChangeType.ADD:
+        case ChangeType.REMOVE:
+          _invalidate();
+          break;
+        case ChangeType.MODIFY:
+          if (_timestamp == null ||
+              fs.file(_path).lastModifiedSync().isAfter(_timestamp!)) {
+            _invalidate();
+          }
+          break;
+      }
+    });
+  }
+
+  Future<void> dispose() {
+    return Future.wait([
+      if (_watcher != null) _watcher!.cancel(),
+      _added.close(),
+      _changed.close(),
+      _removed.close(),
+    ]);
+  }
+
+  Set<String> getKeys() => Set.of(getValues().keys);
+  Map<String, Object> getValues() => _values ??= _readFile();
+
+  Object? getValue(String key) => getValues()[key];
+  Future<void> setValue(String key, Object value) {
+    final values = getValues();
+    values[key] = value;
+    return _saveFile(values);
+  }
+
+  bool? getBool(String key) => getValue(key) as bool?;
+  Future<void> setBool(String key, bool value) {
+    return setValue(key, value);
+  }
+
+  int? getInt(String key) => getValue(key) as int?;
+  Future<void> setInt(String key, int value) {
+    return setValue(key, value);
+  }
+
+  double? getDouble(String key) => getValue(key) as double?;
+  Future<void> setDouble(String key, double value) {
+    return setValue(key, value);
+  }
+
+  String? getString(String key) => getValue(key) as String?;
+  Future<void> setString(String key, String value) {
+    return setValue(key, value);
+  }
+
+  List<String>? getStringList(String key) => getValue(key) as List<String>?;
+  Future<void> setStringList(String key, List<String> value) {
+    return setValue(key, value);
+  }
+
+  Future<void> reset(String key) async {
+    final values = getValues();
+    if (values.remove(key) != null) {
+      _saveFile(values);
+    }
+  }
+
+  void _invalidate() {
+    if (_invalid == true) return;
+    _invalid = true;
+    scheduleMicrotask(() {
+      final oldValues = _values;
+      final newValues = _readFile();
+
+      final newKeys = Set.of(newValues.keys);
+      final oldKeys = Set.of(oldValues?.keys ?? const <String>[]);
+      for (final key in newKeys.difference(oldKeys)) {
+        _added.add(key);
+      }
+      for (final key in oldKeys.difference(newKeys)) {
+        _removed.add(key);
+      }
+
+      final deepEquals = const DeepCollectionEquality().equals;
+      for (final key in newKeys) {
+        final oldValue = oldValues?[key];
+        if (oldValue != null && !deepEquals(oldValue, newValues[key])) {
+          _changed.add(key);
+        }
+      }
+
+      _values = newValues;
+      _invalid = false;
+    });
+  }
+
+  Map<String, Object> _readFile() {
+    final file = fs.file(_path);
+    if (file.existsSync()) {
+      final str = file.readAsStringSync();
+      if (str.isNotEmpty) {
+        final json = jsonDecode(str);
+        if (json is Map) {
+          _timestamp = file.lastModifiedSync();
+          return json.cast<String, Object>();
+        }
+      }
+    }
+    return {};
+  }
+
+  Future<void> _saveFile(Map<String, Object> json) {
+    final file = fs.file(_path);
+    if (!file.existsSync()) {
+      file.createSync(recursive: true);
+    }
+    final str = jsonEncode(json);
+    return file.writeAsString(str);
+  }
+}
