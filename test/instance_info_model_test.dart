c9789f4ec3901362cc23277b9c00d51cd99d6336
Dennis Loose <dennis.loose@canonical.com>
2022-11-21 17:57:04 +0100

    use fake_async in instance info model test

--- test/instance_info_model_test.dart
+++ test/instance_info_model_test.dart
@@ -1,3 +1,4 @@
+import 'package:fake_async/fake_async.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lxd_service/lxd_service.dart';
 import 'package:lxd_test/lxd_test.dart';
@@ -10,32 +11,35 @@ import 'instance_info_model_test.mocks.dart';
 @GenerateMocks([LxdService])
 void main() {
   test('timer', () async {
-    const instanceName = 'foo';
-    const updateInterval = Duration(milliseconds: 10);
-    final instance = testInstance(name: instanceName);
-    final instanceState = testInstanceState();
-    final service = MockLxdService();
+    fakeAsync((async) {
+      const instanceName = 'foo';
+      const updateInterval = Duration(milliseconds: 10);
+      final instance = testInstance(name: instanceName);
+      final instanceState = testInstanceState();
+      final service = MockLxdService();
 
-    when(service.getInstance(instanceName)).thenAnswer((_) async => instance);
-    when(service.getInstanceState(instanceName))
-        .thenAnswer((_) async => instanceState);
-    final model = InstanceInfoModel(
-      instanceName: instanceName,
-      service: service,
-      updateInterval: updateInterval,
-    );
+      when(service.getInstance(instanceName)).thenAnswer((_) async => instance);
+      when(service.getInstanceState(instanceName))
+          .thenAnswer((_) async => instanceState);
+      final model = InstanceInfoModel(
+        instanceName: instanceName,
+        service: service,
+        updateInterval: updateInterval,
+      );
 
-    await model.init();
-    expect(model.instance, instance);
-    expect(model.instanceState, instanceState);
+      model.init();
+      async.elapse(Duration.zero);
+      expect(model.instance, instance);
+      expect(model.instanceState, instanceState);
 
-    var calls = 0;
-    model.addListener(() => calls++);
-    await Future.delayed(updateInterval);
-    await Future.delayed(updateInterval);
-    expect(calls, 2);
-    model.dispose();
-    await Future.delayed(updateInterval);
-    expect(calls, 2);
+      var calls = 0;
+      model.addListener(() => calls++);
+      async.elapse(updateInterval);
+      async.elapse(updateInterval);
+      expect(calls, 2);
+      model.dispose();
+      async.elapse(updateInterval);
+      expect(calls, 2);
+    });
   });
 }
