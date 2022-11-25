d8dc2801b3ac2c6fe7e26c3cf6d707c0bff00e6e
J-P Nurmi <jp.nurmi@canonical.com>
2022-11-25 09:03:46 +0100

    flutter_jsettings: add example snippet

--- packages/flutter_jsettings/README.md
+++ packages/flutter_jsettings/README.md
@@ -9,3 +9,21 @@ A simple JSON key-value store for Flutter.
 
 Similar to [shared_preferences](https://pub.dev/packages/shared_preferences) on
 Linux and Windows but uses a filesystem watcher to provide change notifications.
+
+```dart
+import 'package:flutter_jsettings/flutter_jsettings.dart';
+```
+
+```dart
+ChangeNotifierProvider(
+  create: (_) => JSettingsNotifier('/path/to/settings.json'),
+  child: MyApp(),
+)
+```
+
+```dart
+Checkbox(
+  value: context.watch<JSettingsNotifier>().getBool('key') ?? false,
+  onChanged: (v) => context.read<JSettingsNotifier>().setValue('key', v),
+),
+```
--- packages/flutter_jsettings/lib/flutter_jsettings.dart
+++ packages/flutter_jsettings/lib/flutter_jsettings.dart
@@ -1,3 +1,25 @@
+/// A simple JSON key-value store.
+///
+/// Similar to [shared_preferences](https://pub.dev/packages/shared_preferences) on
+/// Linux and Windows but uses a filesystem watcher to provide change notifications.
+///
+/// ```dart
+/// import 'package:flutter_jsettings/flutter_jsettings.dart';
+/// ```
+///
+/// ```dart
+/// ChangeNotifierProvider(
+///   create: (_) => JSettingsNotifier('/path/to/settings.json'),
+///   child: MyApp(),
+/// )
+/// ```
+///
+/// ```dart
+/// Checkbox(
+///   value: context.watch<JSettingsNotifier>().getBool('key') ?? false,
+///   onChanged: (v) => context.read<JSettingsNotifier>().setValue('key', v),
+/// ),
+/// ```
 library flutter_jsettings;
 
 export 'package:jsettings/jsettings.dart';
