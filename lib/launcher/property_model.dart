9d5680ebe784a33ff02fa8b0a81837e399e2e27e
Dennis Loose <dennis.loose@canonical.com>
2022-11-29 11:39:54 +0100

    make projects available in PropertyModel

--- lib/launcher/property_model.dart
+++ lib/launcher/property_model.dart
@@ -1,4 +1,5 @@
 import 'package:lxd/lxd.dart';
+import 'package:lxd_service/lxd_service.dart';
 import 'package:lxd_x/lxd_x.dart';
 import 'package:petname/petname.dart' as petname;
 import 'package:safe_change_notifier/safe_change_notifier.dart';
@@ -9,18 +10,37 @@ import 'package:safe_change_notifier/safe_change_notifier.dart';
 final _validName = RegExp(r'^[a-zA-Z0-9]([a-zA-Z0-9-]*[a-zA-Z0-9])*$');
 
 class PropertyModel extends SafeChangeNotifier {
-  PropertyModel(this._image) : _name = _image.name ?? generateName();
+  PropertyModel(this._service, this._image)
+      : _name = _image.name ?? generateName();
+
+  final LxdService _service;
 
   String get os => _image.os ?? '';
   String get release => _image.release ?? '';
 
   late final LxdImage _image;
   String _name = '';
+  String _project = 'default';
+
+  var _allProjects = <String>[];
+  List<String> get allProjects => _allProjects;
+
+  Future<void> init() async {
+    _allProjects = await _service.getProjects();
+    notifyListeners();
+  }
 
   String get name => _name;
   set name(String name) {
     if (_name == name) return;
     _name = name;
+    notifyListeners();
+  }
+
+  String get project => _project;
+  set project(String project) {
+    if (_project == project) return;
+    _project = project;
     notifyListeners();
   }
 
