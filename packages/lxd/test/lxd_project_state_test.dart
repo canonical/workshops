7ede1c09119b21160f04d3799379792158478f7a
Dennis Loose <dennis.loose@canonical.com>
2022-11-30 11:14:46 +0100

    vary project state test cases; rename file

--- packages/lxd/test/lxd_project_state_test.dart
+++ packages/lxd/test/lxd_project_state_test.dart
@@ -10,12 +10,12 @@ void main() {
       'resources': {
         'containers': {'Limit': -1, 'Usage': 2},
         'cpu': {'Limit': -1, 'Usage': 0},
-        'disk': {'Limit': -1, 'Usage': 0},
+        'disk': {'Limit': 2, 'Usage': 0},
         'instances': {'Limit': -1, 'Usage': 2},
-        'memory': {'Limit': -1, 'Usage': 0},
-        'networks': {'Limit': -1, 'Usage': 1},
-        'processes': {'Limit': -1, 'Usage': 0},
-        'virtual-machines': {'Limit': -1, 'Usage': 0},
+        'memory': {'Limit': -1, 'Usage': 3},
+        'networks': {'Limit': 10, 'Usage': 1},
+        'processes': {'Limit': -1, 'Usage': 5},
+        'virtual-machines': {'Limit': -1, 'Usage': 10},
       }
     };
 
@@ -30,12 +30,12 @@ void main() {
     expect(state.resources, {
       'containers': LxdProjectStateResource(limit: -1, usage: 2),
       'cpu': LxdProjectStateResource(limit: -1, usage: 0),
-      'disk': LxdProjectStateResource(limit: -1, usage: 0),
+      'disk': LxdProjectStateResource(limit: 2, usage: 0),
       'instances': LxdProjectStateResource(limit: -1, usage: 2),
-      'memory': LxdProjectStateResource(limit: -1, usage: 0),
-      'networks': LxdProjectStateResource(limit: -1, usage: 1),
-      'processes': LxdProjectStateResource(limit: -1, usage: 0),
-      'virtual-machines': LxdProjectStateResource(limit: -1, usage: 0),
+      'memory': LxdProjectStateResource(limit: -1, usage: 3),
+      'networks': LxdProjectStateResource(limit: 10, usage: 1),
+      'processes': LxdProjectStateResource(limit: -1, usage: 5),
+      'virtual-machines': LxdProjectStateResource(limit: -1, usage: 10),
     });
   });
 }
