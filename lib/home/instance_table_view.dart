aedeb297b080ce26320d359f6edd97c783b159c0
J-P Nurmi <jp.nurmi@canonical.com>
2022-12-01 13:03:13 +0100

    Instance table: implement keyboard search/filter

--- lib/home/instance_table_view.dart
+++ lib/home/instance_table_view.dart
@@ -1,6 +1,7 @@
 import 'package:collection/collection.dart';
 import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:yaru_icons/yaru_icons.dart';
@@ -30,6 +31,8 @@ class _InstanceTableView extends StatefulWidget {
 }
 
 class _InstanceTableViewState extends State<_InstanceTableView> {
+  final _searchFocusNode = FocusNode();
+
   @override
   void initState() {
     super.initState();
@@ -39,65 +42,122 @@ class _InstanceTableViewState extends State<_InstanceTableView> {
   }
 
   @override
+  void dispose() {
+    _searchFocusNode.dispose();
+    super.dispose();
+  }
+
+  KeyEventResult _handleKeyEvent(FocusNode focusNode, KeyEvent event) {
+    final filter = event.character?.trim();
+    if (!_searchFocusNode.hasFocus &&
+        filter?.isNotEmpty == true &&
+        !LogicalKeyboardKey.isControlCharacter(filter!)) {
+      final model = context.read<InstanceTableModel>();
+      model.filter(filter);
+      _searchFocusNode.requestFocus();
+      return KeyEventResult.handled;
+    }
+    return KeyEventResult.ignored;
+  }
+
+  void _resetSearch() {
+    final model = context.read<InstanceTableModel>();
+    model.filter('');
+    _searchFocusNode.unfocus(
+        disposition: UnfocusDisposition.previouslyFocusedChild);
+  }
+
+  @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context);
     final model = context.watch<InstanceTableModel>();
-    return DataTable2(
-      headingRowColor:
-          MaterialStateProperty.all(Theme.of(context).backgroundColor),
-      sortArrowIcon: YaruIcons.pan_up,
-      sortAscending: model.sortAscending,
-      sortColumnIndex: model.sortColumn.index,
-      columnSpacing: 10,
-      horizontalMargin: 10,
-      headingRowHeight: 36,
-      dataRowHeight: kMinInteractiveDimension,
-      columns: [
-        DataColumn2(
-          label: Padding(
-            padding: const EdgeInsetsDirectional.only(end: 4),
-            child: Text(l10n.instanceLabel),
-          ),
-          onSort: model.sort,
-        ),
-        if (model.selectedProjects.length > 1)
-          DataColumn2(
-            label: Padding(
-              padding: const EdgeInsetsDirectional.only(end: 4),
-              child: Text(l10n.projectLabel),
+    return Focus(
+      autofocus: true,
+      onKeyEvent: _handleKeyEvent,
+      child: Scaffold(
+        body: DataTable2(
+          headingRowColor:
+              MaterialStateProperty.all(Theme.of(context).backgroundColor),
+          sortArrowIcon: YaruIcons.pan_up,
+          sortAscending: model.sortAscending,
+          sortColumnIndex: model.sortColumn.index,
+          columnSpacing: 10,
+          horizontalMargin: 10,
+          headingRowHeight: 36,
+          dataRowHeight: kMinInteractiveDimension,
+          columns: [
+            DataColumn2(
+              label: Padding(
+                padding: const EdgeInsetsDirectional.only(end: 4),
+                child: Text(l10n.instanceLabel),
+              ),
+              onSort: model.sort,
             ),
-            fixedWidth: 200,
-            onSort: model.sort,
-          ),
-        DataColumn2(
-          label: Padding(
-            padding: const EdgeInsetsDirectional.only(start: 2 * 36),
-            child: PopupMenuButton(
-              iconSize: 16,
-              tooltip: '',
-              itemBuilder: (context) {
-                return [
-                  for (final project in model.projects)
-                    YaruMultiSelectPopupMenuItem(
-                      checked: model.isProjectSelected(project),
-                      child: Text(project),
-                      onChanged: (value) => model.selectProject(project, value),
+            if (model.selectedProjects.length > 1)
+              DataColumn2(
+                label: Padding(
+                  padding: const EdgeInsetsDirectional.only(end: 4),
+                  child: Text(l10n.projectLabel),
+                ),
+                fixedWidth: 200,
+                onSort: model.sort,
+              ),
+            DataColumn2(
+              label: Padding(
+                padding: const EdgeInsetsDirectional.only(start: 2 * 36),
+                child: PopupMenuButton(
+                  iconSize: 16,
+                  tooltip: '',
+                  itemBuilder: (context) {
+                    return [
+                      for (final project in model.projects)
+                        YaruMultiSelectPopupMenuItem(
+                          checked: model.isProjectSelected(project),
+                          child: Text(project),
+                          onChanged: (value) =>
+                              model.selectProject(project, value),
+                        ),
+                    ];
+                  },
+                  icon: const Icon(Icons.filter_list),
+                ),
+              ),
+              fixedWidth: 120,
+            ),
+          ],
+          rows: model.instances.mapIndexed((index, id) {
+            return buildInstanceTableRow(
+              context,
+              index: index,
+              id: id,
+            );
+          }).toList(),
+        ),
+        bottomNavigationBar: model.filterKey.isNotEmpty
+            ? CallbackShortcuts(
+                bindings: {
+                  const SingleActivator(LogicalKeyboardKey.escape):
+                      _resetSearch,
+                },
+                child: TextFormField(
+                  initialValue: model.filterKey,
+                  focusNode: _searchFocusNode,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(4),
+                        topRight: Radius.circular(4),
+                        bottomLeft: Radius.circular(12),
+                        bottomRight: Radius.circular(12),
+                      ),
                     ),
-                ];
-              },
-              icon: const Icon(Icons.filter_list),
-            ),
-          ),
-          fixedWidth: 120,
-        ),
-      ],
-      rows: model.instances.mapIndexed((index, id) {
-        return buildInstanceTableRow(
-          context,
-          index: index,
-          id: id,
-        );
-      }).toList(),
+                  ),
+                  onChanged: model.filter,
+                  onEditingComplete: _resetSearch,
+                ),
+              )
+            : null,
+      ),
     );
   }
 }
